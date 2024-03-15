d676681558826c61b44241b7a3a08a0ffd21a999
karishmaMohammed <karishmamohammed43@gmail.com>
2024-03-15 16:16:00 +0530

    changes

--- src/Components/BottomRoute.js
+++ src/Components/BottomRoute.js
@@ -1,52 +1,60 @@
-import React, { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { motion } from 'framer-motion'
-import { useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+import { useLocation } from "react-router-dom";
 
 function BottomRoute({ data }) {
+  const [isFlipped, setIsFlipped] = useState(false);
+  const navigate = useNavigate();
+  const handleNavigate = (item) => {
+    navigate(item);
+  };
 
-    const [isFlipped, setIsFlipped] = useState(false);
-    const navigate = useNavigate()
-    const handleNavigate = (item) => {
-        navigate(item)
-    }
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: -100 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ type: "spring", duration: 5 }}
+    >
+      <div className="intro-flipping-cards" style={{ background: "white" }}>
+        {data.map((item, index) => (
+          <div
+            key={index}
+            className={`intro-flip-card ${isFlipped ? "flipped" : ""}`}
+            onClick={handleFlip}
+          >
+            <div className="intro-flip-card-inner">
+              <div
+                className="intro-flip-card-front"
+                style={{ background: item.frontColor }}
+              >
+                <img
+                  src={item.frontLogo}
+                  style={{ width: "50px", height: "50px" }}
+                />
+                <h2>{item.front}</h2>
+              </div>
 
-
-
-
-    const handleFlip = () => {
-        setIsFlipped(!isFlipped);
-    };
-    return (
-        <motion.div initial={{ opacity: 0, x: -100 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ type: "spring", duration: 5 }}>
-            <div className='intro-flipping-cards' style={{ background: 'white' }} >
-                {data.map((item, index) => (
-                    <div key={index} className={`intro-flip-card ${isFlipped ? 'flipped' : ''}`} onClick={handleFlip}>
-
-                        <div className="intro-flip-card-inner">
-                            <div className="intro-flip-card-front" style={{ background: item.frontColor }}>
-                                <img src={item.frontLogo} style={{ width: '50px', height: '50px' }} />
-                                <h2>{item.front}</h2>
-                            </div>
-
-                            <div className="intro-flip-card-back" style={{ background: item.backColor }} onClick={() => handleNavigate(item.navigateTo)}>
-
-                                <img src={item.backLogo} style={{ width: '50px', height: '50px' }} />
-                                <h2>click to view</h2>
-
-                            </div>
-
-
-                        </div>
-
-
-                    </div>
-                ))}
+              <div
+                className="intro-flip-card-back"
+                style={{ background: item.backColor }}
+                onClick={() => handleNavigate(item.navigateTo)}
+              >
+                <img
+                  src={item.backLogo}
+                  style={{ width: "50px", height: "50px" }}
+                />
+                <h2>click to view</h2>
+              </div>
             </div>
-        </motion.div>
-    )
+          </div>
+        ))}
+      </div>
+    </motion.div>
+  );
 }
 
-export default BottomRoute
+export default BottomRoute;
