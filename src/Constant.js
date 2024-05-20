954c0fc58c738acd1d431864b19e8055430ba788
karishmaMohammed <karishmamohammed43@gmail.com>
2024-05-20 12:46:58 +0530

    modifications done

--- src/Constant.js
+++ src/Constant.js
@@ -1,2 +1,3 @@
 export const ASSETS_URL = 'https://portfolio-ks.s3.ap-south-1.amazonaws.com/'
-export const BASE_URL = 'http://localhost:7000'
+// export const BASE_URL = 'http://localhost:7000'
+export const BASE_URL = 'https://portfolio-backend-sktj.onrender.com'
